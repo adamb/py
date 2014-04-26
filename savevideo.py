09272a9cb461cc4fead214c75ebbaab061cf3983
Adam Beguelin <adam@sensr.net>
2014-04-26 10:06:45 -0700

    this works after reinstalling opencv --with-ffmpeg

--- savevideo.py
+++ savevideo.py
@@ -1,24 +1,22 @@
 import numpy as np
 import cv2
 
-# capSize = (1028,720) # this is the size of my source video
-# fourcc = cv2.cv.CV_FOURCC('m', 'p', '4', 'v') # note the lower case
-# self.vout = cv2.VideoWriter()
-# success = self.vout.open('output.mov',fourcc,fps,capSize,True)
-
-
 cap = cv2.VideoCapture(0)
 
 # Define the codec and create VideoWriter object
 fps = 15
+capSize = (640,480)
 fourcc = cv2.cv.CV_FOURCC('m','p','4','v')
 out = cv2.VideoWriter()
-suc = out.open('output.mov',fourcc, fps, (640,480),True)
+suc = out.open('output.mov',fourcc, fps, capSize,True)
 
 while(cap.isOpened()):
     ret, frame = cap.read()
+    width = np.size(frame, 1) #here is why you need numpy!  (remember to "import numpy as np")
+    height = np.size(frame, 0)
+    print width, height
     if ret==True:
-        frame = cv2.flip(frame,0)
+        # frame = cv2.flip(frame,0)
 
         # write the flipped frame
         out.write(frame)
